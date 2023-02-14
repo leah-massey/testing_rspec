5d6518cbc68ba70f4bb7cdd5c1cb71fabcbf50c5
Leah Massey <leahmassey@MASTUDE077WR31G.cust.communityfibre.co.uk>
2023-02-14 16:42:20 +0000

    check_codeword spec completed

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,2 +1,15 @@
 require 'check_codeword'
 RSpec.describe "check_codeword method" do
+    it "checks the codeword" do 
+      result = check_codeword("horse")
+      expect(result).to eq "Correct! Come in."
+    end
+    it "checks the codeword" do
+      result = check_codeword("he")
+      expect(result).to eq "Close, but nope."
+    end
+    it "checks the codeword" do 
+      result = check_codeword("egg")
+      expect(result).to eq "WRONG!"
+    end
+end
